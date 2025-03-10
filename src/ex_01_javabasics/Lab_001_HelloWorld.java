0c6e98132f1b6f971bbc1cb38842deb116476055
Rachana <rachanakore9@outlook.com>
2025-03-10 17:23:56 +0530

    bbpractise

---
+++ src/ex_01_javabasics/Lab_001_HelloWorld.java
@@ -0,0 +1,10 @@
+package ex_01_javabasics;
+
+public class Lab_001_HelloWorld {
+    //main
+    public static void main(String[] args) {
+
+        //sout
+        System.out.println("hey");
+    }
+}
